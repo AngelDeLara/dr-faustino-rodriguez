a723ac056a135fb2a456d46ef68e8f0d8766d701
AngelDeLara <robertoangel19@gmail.com>
2024-12-02 22:11:33 -0600

    r

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -10,7 +10,7 @@ const About: React.FC = () => {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in dui mauris. 
             Vivamus hendrerit arcu sed erat molestie vehicula. Sed auctor neque eu tellus 
             rhoncus ut eleifend nibh porttitor. Ut in nulla enim. Phasellus molestie magna 
-            non est bibendum non venenatis nisl tempor. Suspendisse dictum feugiat nisl ut dapibus.
+            non est bibendum non venenatis nisl tempor. Suspendisse dictum feugiat nisl ut dapibus
           </p>
         </div>
         <div className="flex-1 overflow-hidden">
